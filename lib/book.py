ed09ebccbdd04c6ce075b5ca34d9531ea794c2d0
Camrin <camrindavis58@gmail.com>
2025-12-03 14:45:56 -0600

    Refactor Book and Coffee classes to use __setattr__ for validation of attributes

--- lib/book.py
+++ lib/book.py
@@ -3,12 +3,16 @@
 class Book:
     '''A Book class'''
     def __init__(self, title, page_count):
+        # use __setattr__ for validation
         self.title = title
-        # Ensure page_count is an integer
-        if isinstance(page_count, int):
-            self.page_count = page_count
-        else:
+        self.page_count = page_count
+
+    def __setattr__(self, name, value):
+        if name == "page_count" and not isinstance(value, int):
             print("page_count must be an integer")
-# Make sure to add a method to turn the page
+        # always set the value
+        super().__setattr__(name, value)
+
+    # Make sure to add a method to turn the page
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-#  define __init__ with size and price parameters
+    #  define __init__ with size and price parameters
     def __init__(self, size, price):
-        if size in ["Small", "Medium", "Large"]:
-            self.size = size
-        else:
-            print("size must be Small, Medium, or Large")
+        self.size = size
         self.price = price
-# define tip method that adds 1 to price and prints a message
+
+    def __setattr__(self, name, value):
+        if name == "size" and value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
+        super().__setattr__(name, value)
+
+    # define tip method that adds 1 to price and prints a message
     def tip(self):
         print("This coffee is great, here’s a tip!")
         self.price += 1
